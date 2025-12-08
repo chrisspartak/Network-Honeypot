8de21c92e4671189568c784fefc5141827c6a5fa
Al Nikolov <roottoorfieuorg@gmail.com>
2025-12-08 14:52:31 +0200

    Plan. Fake FTP notes.
    Co-authored-by: chrisspartak <chrisspartak@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,7 @@
+# 1. Fake FTP service
+# https://datatracker.ietf.org/doc/html/rfc959
+# (FTP basic authentication)
+# https://docs.python.org/3/library/asyncio.html
+#
+# 2. Port mapping and scanning response to the attacker's IP address
+# 3. Email reporting
